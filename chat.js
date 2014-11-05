ff29ca509092dfe34caaf9ab475920ca590ab7b7
pinkstarfishofdeath <pinkstarfishofdeath@gmx.com>
2014-11-04 16:06:01 -0800

    fixed "msg" field reset

--- chat.js
+++ chat.js
@@ -28,7 +28,7 @@ var ChatEngine=function(){
           chatZone.innerHTML+='<div class="chatmsg"><b>'+name+'</b>: '+msg+'<br/></div>';
           oldata='<div class="chatmsg"><b>'+name+'</b>: '+msg+'<br/></div>';          
           this.ajaxSent();
-            document.getElementById("msg").reset();
+            document.getElementById("msg").value = "";
           return false;
      };
      //sending message to server
